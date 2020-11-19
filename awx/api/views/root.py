f4b241ee01f052fcd9d22227ecc9cea5a4d6719b
Jim Ladd <jladd@redhat.com>
2020-11-19 12:58:37 -0800

    drop: settings_registry.is_setting_read_only()

--- awx/api/views/root.py
+++ awx/api/views/root.py
@@ -382,8 +382,10 @@ class ApiV2ConfigView(APIView):
 
         # If the license is valid, write it to the database.
         if license_data_validated['valid_key']:
-            if not settings_registry.is_setting_read_only('TOWER_URL_BASE'):
-                settings.TOWER_URL_BASE = "{}://{}".format(request.scheme, request.get_host())
+            # *** Dropping the following condition - we think this may be
+            # preventing TOWER_URL_BASE from getting set ***
+            # if not settings_registry.is_setting_read_only('TOWER_URL_BASE'):
+            settings.TOWER_URL_BASE = "{}://{}".format(request.scheme, request.get_host())
             return Response(license_data_validated)
 
         logger.warning(smart_text(u"Invalid subscription submitted."),
